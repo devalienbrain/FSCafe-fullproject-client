3ad56d7651c403cf179b2a3df59a2233f2d11751
Alien Brain <murad.dub@gmail.com>
2023-11-15 13:07:45 +0600

    dark light theme  toggle style added

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FiSun } from "react-icons/fi";
+import { MdDarkMode } from "react-icons/md";
 
 const Navbar = () => {
   const navOptions = (
@@ -17,8 +20,38 @@ const Navbar = () => {
       </li>
     </>
   );
+
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    const html = document.documentElement;
+    if (theme === "light") {
+      html.setAttribute("data-theme", "dark");
+      html.classList.remove("light");
+      html.classList.add("dark");
+      setTheme("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      html.setAttribute("data-theme", "light");
+      html.classList.remove("dark");
+      html.classList.add("light");
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+    }
+    // console.log("Previous Theme:", theme);
+  };
+
+  useEffect(() => {
+    const localStorageTheme = localStorage.getItem("theme") || "light";
+    setTheme(localStorageTheme);
+    const html = document.documentElement;
+    html.setAttribute("data-theme", localStorageTheme);
+    html.classList.add(localStorageTheme);
+    // console.log("Local Storage Theme:", localStorageTheme);
+  }, []);
+
   return (
-    <div className="navbar fixed z-10 bg-opacity-20 bg-black text-lime-500 container mx-auto">
+    <div className="navbar fixed z-10 bg-opacity-20 bg-black text-yellow-700 container mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -53,6 +86,33 @@ const Navbar = () => {
         <a className="px-7 py-3 uppercase bg-black text-white font-semibold text-sm shadow-lg hover:shadow-2xl">
           Join with us
         </a>
+        <div className="px-2 py-1 rounded-2xl bg-blue-400">
+          <div
+            onClick={toggleTheme}
+            className={`mr-2 rounded-full shadow-2xl w-4 h-4 text-xs bg-black ${
+              theme === "light" ? "hidden" : ""
+            }`}
+          ></div>
+          <div
+            onClick={toggleTheme}
+            className={`ml-2 rounded-full shadow-2xl w-4 h-4 text-xs bg-white ${
+              theme === "dark" ? "hidden" : ""
+            }`}
+          ></div>
+        </div>
+        {/* <div>
+          {theme === "light" ? (
+            <MdDarkMode
+              onClick={toggleTheme}
+              className="ml-2 rounded-full w-5 h-5 text-xs"
+            ></MdDarkMode>
+          ) : (
+            <FiSun
+              onClick={toggleTheme}
+              className="ml-2 rounded-full w-5 h-5 text-xs"
+            ></FiSun>
+          )}
+        </div> */}
       </div>
     </div>
   );
